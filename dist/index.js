4b13b1ada7f3a39f9b18904232cd65aeb4694ac4
afeiship <1290657123@qq.com>
2017-04-28 00:30:29 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -13,9 +13,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.default = function (inApiPath) {
   return function (ctx, next) {
     var urlData = _url2.default.parse(ctx.url);
-    var api = urlData.pathname.split('/').pop();
+    var api = (urlData.pathname.split('/') || []).pop();
     var data = ctx.request.body;
-    ctx.body = require(inApiPath + '/' + api).default(ctx, data);
+    ctx.body = api && require(inApiPath + '/' + api).default(ctx, data);
     next();
   };
 };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import url from 'url';
 
 export default (inApiPath) => (ctx, next) => {
   const urlData = url.parse(ctx.url);
-  const api = urlData.pathname.split('/').pop();
+  const api = (urlData.pathname.split('/') || []).pop();
   const data = ctx.request.body;
-  ctx.body = require(`${inApiPath}/${api}`).default(ctx, data);
+  ctx.body = api && require(`${inApiPath}/${api}`).default(ctx, data);
   next();
 };
