b92546931e3ff74e0a7202ee7bc810cdb41b190d
afeiship <1290657123@qq.com>
2017-06-26 17:55:57 +0800

    apiFile->api-file

--- dist/index.js
+++ dist/index.js
@@ -10,13 +10,17 @@ var _url2 = _interopRequireDefault(_url);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function dasherize(inStr) {
+  return inStr.replace(/::/g, '/').replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2').replace(/([a-z\d])([A-Z])/g, '$1_$2').replace(/_/g, '-').toLowerCase();
+};
+
 exports.default = function (inApiPath) {
   return function (ctx, next) {
     var urlData = _url2.default.parse(ctx.url);
     var api = (urlData.pathname.split('/') || []).pop();
     var data = ctx.request.body;
     try {
-      ctx.body = require(inApiPath + '/' + api).default(ctx, data);
+      ctx.body = require(inApiPath + '/' + dasherize(api)).default(ctx, data);
     } catch (_) {
       ctx.body = '[NODE ERROR]: API ERROR~';
     }
