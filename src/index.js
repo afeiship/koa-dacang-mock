1e9274b8d7232183b72e83fc742b7b3309932593
afeiship <1290657123@qq.com>
2017-06-30 17:50:11 +0800

    issue: api mapping file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import url from 'url';
 
-function dasherize(inStr) {
-  return inStr.replace(/::/g, '/')
-    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
-    .replace(/([a-z\d])([A-Z])/g, '$1_$2')
-    .replace(/_/g, '-')
-    .toLowerCase()
-};
-
 export default (inApiPath) => (ctx, next) => {
   const urlData = url.parse(ctx.url);
   const api = (urlData.pathname.split('/') || []).pop();
   const data = ctx.request.body;
   try {
-    ctx.body = require(`${inApiPath}/${dasherize(api)}`).default(ctx, data);
+    ctx.body = require(`${inApiPath}/${api}`).default(ctx, data);
   } catch (_) {
     ctx.body = '[NODE ERROR]: API ERROR~';
   }
